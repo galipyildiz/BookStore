21efd14ef450b5a423085cdcd6fb56ac1dd9e403
galip975 <70694743+galip975@users.noreply.github.com>
2021-04-28 11:57:36 +0300

    Application DbContext Mapping

---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            //builder.ToTable("Urunler"); tablo ismi değiştirmek için
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasOne(x => x.Author)
+                .WithMany()
+                .HasForeignKey(x=>x.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);//bağlıları silinsin
+
+            builder.HasOne(x => x.Category)
+                .WithMany()
+                .HasForeignKey(x => x.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);//bağlıları silme hata fırlatıcak
+        }
+    }
+}
