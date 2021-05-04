6a63c6832fdb12422edb0523d945cad5bbf85f34
galip975 <70694743+galip975@users.noreply.github.com>
2021-05-04 13:06:10 +0300

    add order basket entities

---
+++ src/ApplicationCore/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string BuyerId { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public Address ShiptoAddress { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
